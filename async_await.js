99d9829218822a530311fbab9e54151784542815
janocl <janocl84@gmail.com>
2018-12-15 20:52:48 -0300

    agregando algunos cambios

--- async_await.js
+++ async_await.js
@@ -2,19 +2,22 @@
 let getNombre = () =>{
     return new Promise((resolv, reject)=>{
         setTimeout(()=>{
-            resolv('Fernanda!');
+            resolv('Mauricio!');
         }, 3000)
     })
 }
-
-let saludo = async() => {
-    let nombre = await getNombre()
-    return `Hola ${nombre}!`;
-}
-
-
 // getNombre().then(nombre => {
 //     console.log(nombre)
 // }).catch(e => {
 //     console.log('Error en async ', e)
 // })
+
+let saludo = async() => {
+    let nombre = await getNombre()
+    return `Hola ${nombre}.-`;
+}
+
+
+saludo().then(resp => {
+    console.log(resp)
+})
