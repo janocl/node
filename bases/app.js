3974e86b660ae59cc274db82a82668cca79a8161
janocl <janocl84@gmail.com>
2018-12-29 01:58:13 -0300

    Se agrega validacion si no es un numero para la base

--- bases/app.js
+++ bases/app.js
@@ -2,6 +2,6 @@ const {crearArchivo} = require('./multiplicar/multiplicar')
 
 let base = 3;
 
-crearArchivo(base).then( archivo =>{
-    console.log(`Archivo creado: ${archivo}!`);
-})
+crearArchivo(base)
+    .then( archivo => console.log(`Archivo creado: ${archivo}!`))
+    .catch(e => console.log(e));
--- bases/multiplicar/multiplicar.js
+++ bases/multiplicar/multiplicar.js
@@ -4,22 +4,22 @@ let crearArchivo = (base) => {
 
     return new Promise((resolv, reject)=>{
 
+        if(!Number(base)){
+            return reject(`El valor en base "${base}", no es un numero!`)
+        }
+
         let data = '';
         for(let i=1; i<=10; i++){
             data += `${base} * ${i} = ${base*i}\n`;
         }
 
         fs.writeFile(`tabla-${base}.txt`, data, (err) => {
-
             if (err)
                 reject(err)
             else
                 resolv(`tabla-${base}.txt`)
-
         });
-
     })
-
 }
 
 module.exports = {
