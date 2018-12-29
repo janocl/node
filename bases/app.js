1ec46debbd7a45d96f261dd10e412aae63fa21d4
janocl <janocl84@gmail.com>
2018-12-29 01:35:49 -0300

    Se agrega la forma basica de importar archivos a nuestro proyecto

--- bases/app.js
+++ bases/app.js
@@ -1,12 +1,7 @@
-const fs = require('fs')
+const {crearArchivo} = require('./multiplicar/multiplicar')
 
 let base = 2;
-let data = '';
-for(let i=1; i<=10; i++){
-    data += `${base} * ${i} = ${base*i}\n`;
-}
 
-fs.writeFile(`tabla-${base}.txt`, data, (err) => {
-  if (err) throw err;
-  console.log(`El archivo tabla-${base}.txt ha sido creado!`);
-});
+crearArchivo(base).then( archivo =>{
+    console.log(`Archivo creado: ${archivo}!`);
+})
--- bases/multiplicar/multiplicar.js
+++ bases/multiplicar/multiplicar.js
@@ -1,11 +1,27 @@
 const fs = require('fs')
 
-let data = '';
-for(let i=1; i<=10; i++){
-    data += `${base} * ${i} = ${base*i}\n`;
+let crearArchivo = (base) => {
+
+    return new Promise((resolv, reject)=>{
+
+        let data = '';
+        for(let i=1; i<=10; i++){
+            data += `${base} * ${i} = ${base*i}\n`;
+        }
+
+        fs.writeFile(`tabla-${base}.txt`, data, (err) => {
+
+            if (err)
+                reject(err)
+            else
+                resolv(`tabla-${base}.txt`)
+
+        });
+        
+    })
+
 }
 
-fs.writeFile(`tabla-${base}.txt`, data, (err) => {
-  if (err) throw err;
-  console.log(`El archivo tabla-${base}.txt ha sido creado!`);
-});
+module.exports{
+    crearArchivo
+}
